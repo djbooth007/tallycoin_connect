7a9d895794af054b7646de91cd5ca7cfb6ab369c
DJ Booth <djbooth007@gmail.com>
2020-07-31 09:51:49 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 import os
 import socket
 
+# get LND keys and save to file
+
 stream = os.popen("base64 /home/bitcoin/.lnd/tls.cert | tr -d '\n'")
 cert = stream.read()
 
@@ -9,7 +11,19 @@ macaroon = stream.read()
 
 json =  '{ "tallycoin_api":"", "tls_cert":"'+cert+'", "macaroon":"'+macaroon+'"}'
 
+# write keys
+
 fd = open("tallycoin_api.key","w")
 fd.write(json)
+
+# setup always-on service
+
+stream = os.popen("cat tallycoin_connect.service");
+service = stream.read()
+
+fd = open("/etc/systemd/system/tallycoin_connect.service","w")
+fd.write(service)
+
+stream = os.popen("systemctl enable dots.service");
 
 print("Enter your API key at http://"+socket.gethostbyname(socket.gethostname())+":8123/" )
