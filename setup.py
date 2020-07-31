4fd2b35181c0f76b7583c466354460a08a37f705
DJ Booth <djbooth007@gmail.com>
2020-07-31 11:19:14 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,38 @@
 import os
+from os import path
 import socket
-
-# get LND keys and save to file
-
-stream = os.popen("base64 /home/bitcoin/.lnd/tls.cert | tr -d '\n'")
-cert = stream.read()
-
-stream = os.popen("base64 /home/bitcoin/.lnd/data/chain/bitcoin/mainnet/admin.macaroon | tr -d '\n'")
-macaroon = stream.read()
-
-json =  '{ "tallycoin_api":"", "tls_cert":"'+cert+'", "macaroon":"'+macaroon+'"}'
-
-# write keys
-
-fd = open("tallycoin_api.key","w")
-fd.write(json)
+import json
 
 # setup always-on service
 
-stream = os.popen("cat tallycoin_connect.service");
-service = stream.read()
+os.popen("cp tallycoin_connect.service /etc/systemd/system/").read();
+os.popen("systemctl daemon-reload").read();
 
-fd = open("/etc/systemd/system/tallycoin_connect.service","w")
-fd.write(service)
+os.popen("systemctl enable tallycoin_connect.service").read();
+os.popen("systemctl start tallycoin_connect.service").read(); 
 
-stream = os.popen("systemctl enable dots.service");
+# get LND keys and save to file
 
-print("Enter your API key at http://"+socket.gethostbyname(socket.gethostname())+":8123/" )
+stream = os.popen("base64 /home/bitcoin/.lnd/tls.cert | tr -d '\n'");
+cert = stream.read();
+
+stream = os.popen("base64 /home/bitcoin/.lnd/data/chain/bitcoin/mainnet/admin.macaroon | tr -d '\n'");
+macaroon = stream.read();
+
+if path.exists("tallycoin_api.key"):
+  stream = os.popen("cat tallycoin_api.key");
+  k = stream.read();
+  key = json.loads(k);
+  key = key['tallycoin_api'];
+else:  
+  key = ''; 
+
+json =  '{ "tallycoin_api":"'+key+'", "tls_cert":"'+cert+'", "macaroon":"'+macaroon+'"}';
+
+# write keys
+
+fd = open("tallycoin_api.key","w");
+fd.write(json);
+os.chmod("tallycoin_api.key", 0o777);
+
+print("Enter your API key at http://"+socket.gethostbyname(socket.gethostname())+":8123/" );
